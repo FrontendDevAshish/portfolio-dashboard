18e2ef62a98edc42fde951faaf8c89486f2dcbcb
Ashish <kumar.29ashish.95.@gmail.com>
2024-09-21 16:16:02 +0530

    icon fix

--- app/components/Approach.tsx
+++ app/components/Approach.tsx
@@ -186,18 +186,22 @@ const AceternityIcon = ({ order }: { order: string }) => {
   );
 };
 
-export const Icon = ({ className, ...rest }: any) => {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth="1.5"
-      stroke="currentColor"
-      className={className}
-      {...rest}
-    >
-      <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v12m6-6H6" />
-    </svg>
-  );
-};
+interface IconProps extends React.SVGProps<SVGSVGElement> {
+    className?: string;
+  }
+  
+  export const Icon: React.FC<IconProps> = ({ className, ...rest }) => {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth="1.5"
+        stroke="currentColor"
+        className={className}
+        {...rest}
+      >
+        <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v12m6-6H6" />
+      </svg>
+    );
+  };
